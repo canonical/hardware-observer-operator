ec5f53407cf112f833e271025cec6f14f679eac5
jneo8 <james.lin@canonical.com>
2023-06-05 13:48:30 +0800

    test(tests): Add basic functional test for build & deploy

--- tests/functional/test_charm.py
+++ tests/functional/test_charm.py
@@ -15,9 +15,13 @@ logger = logging.getLogger(__name__)
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = METADATA["name"]
 
+PRINCIPAL_APP_NAME = "ubuntu"
+
+TIMEOUT = 600
+
 
 @pytest.mark.abort_on_fail
-async def test_build_and_deploy(ops_test: OpsTest):
+async def test_build_and_deploy(ops_test: OpsTest, series):
     """Build the charm-under-test and deploy it together with related charms.
 
     Assert on the unit status before any relations/configurations take place.
@@ -25,10 +29,23 @@ async def test_build_and_deploy(ops_test: OpsTest):
     # Build and deploy charm from local source folder
     charm = await ops_test.build_charm(".")
 
-    # Deploy the charm and wait for active/idle status
     await asyncio.gather(
-        ops_test.model.deploy(charm, application_name=APP_NAME),
-        ops_test.model.wait_for_idle(
-            apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=1000
+        ops_test.model.deploy(
+            PRINCIPAL_APP_NAME,
+            application_name=PRINCIPAL_APP_NAME,
+            channel="stable",
+            series=series,
         ),
+        ops_test.model.wait_for_idle(
+            apps=[PRINCIPAL_APP_NAME], status="active", raise_on_blocked=True, timeout=TIMEOUT
+        ),
+    )
+
+    # Deploy the charm and wait for active/idle status
+    await ops_test.model.deploy(charm, application_name=APP_NAME, num_units=0)
+    await ops_test.model.add_relation(
+        f"{PRINCIPAL_APP_NAME}:juju-info", f"{APP_NAME}:general-info"
+    )
+    await ops_test.model.wait_for_idle(
+        apps=[APP_NAME], status="active", raise_on_blocked=True, timeout=TIMEOUT
     )
