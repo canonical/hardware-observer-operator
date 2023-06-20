b3d34b734e156514536869bb817101117f5586b3
jneo8 <james.lin@canonical.com>
2023-06-20 18:03:45 +0800

    feat(vendor): Add 3rd party vendor's resources install/remove strategy
    - perccli
    - sas2ircu
    - sas3ircu

--- src/vendor.py
+++ src/vendor.py
@@ -2,6 +2,7 @@
 import logging
 import os
 import shutil
+import stat
 import subprocess
 import typing as t
 from abc import ABCMeta, abstractmethod
@@ -53,8 +54,24 @@ def remove_deb(pkg: str) -> None:
         raise apt.PackageError(f"Fail to remove deb {pkg}") from exc
 
 
-class VendorStrategyABC(metaclass=ABCMeta):
-    """Basic install strategy class."""
+def make_executable(src: Path) -> None:
+    """Make src executable."""
+    uid = 0
+    gid = 0
+    try:
+        os.chmod(src, stat.S_IEXEC)
+        os.chown(src, uid, gid)
+    except OSError as err:
+        logger.error(err)
+        raise err
+
+
+class StrategyABC(metaclass=ABCMeta):  # pylint: disable=R0903
+    """Base strategy."""
+
+
+class TPRStrategyABC(StrategyABC, metaclass=ABCMeta):
+    """Third party resource strategy class."""
 
     @abstractmethod
     def install(self, name: str, path: Path) -> None:
@@ -64,7 +81,8 @@ class VendorStrategyABC(metaclass=ABCMeta):
     def remove(self) -> None:
         """Installation details."""
 
-class StorCLIStrategy(VendorStrategyABC):
+
+class StorCLIStrategy(TPRStrategyABC):
     """Strategy to install storcli."""
 
     name = "storcli"
@@ -72,60 +90,102 @@ class StorCLIStrategy(VendorStrategyABC):
     symlink_bin = TOOLS_DIR / "storcli"
 
     def install(self, name: str, path: Path) -> None:
-        """Install storecli."""
+        """Install storcli."""
         install_deb(name, path)
         symlink(src=self.origin_path, dst=self.symlink_bin)
 
     def remove(self) -> None:
-        """Remove storecli."""
+        """Remove storcli."""
         self.symlink_bin.unlink(missing_ok=True)
         logger.debug("Remove file %s", self.symlink_bin)
         remove_deb(pkg=self.name)
+
+
+class PercCLIStrategy(TPRStrategyABC):
+    """Strategy to install storcli."""
+
+    name = "perccli"
+    origin_path = Path("/opt/MegaRAID/perccli/perccli64")
+    symlink_bin = TOOLS_DIR / "perccli"
+
+    def install(self, name: str, path: Path) -> None:
+        """Install perccli."""
+        install_deb(name, path)
+        symlink(src=self.origin_path, dst=self.symlink_bin)
+
+    def remove(self) -> None:
+        """Remove perccli."""
+        self.symlink_bin.unlink(missing_ok=True)
+        logger.debug("Remove file %s", self.symlink_bin)
+        remove_deb(pkg=self.name)
+
+
+class SAS2IRCUStrategy(TPRStrategyABC):
+    """Strategy to install storcli."""
+
+    name = "sas2ircu"
+    symlink_bin = TOOLS_DIR / "sas2ircu"
+
+    def install(self, name: str, path: Path) -> None:
+        """Install sas2ircu."""
+        make_executable(path)
+        symlink(src=path, dst=self.symlink_bin)
+
+    def remove(self) -> None:
+        """Remove sas2ircu."""
+        self.symlink_bin.unlink(missing_ok=True)
+        logger.debug("Remove file %s", self.symlink_bin)
+
+
+class SAS3IRCUStrategy(SAS2IRCUStrategy):
+    """Strategy to install storcli."""
+
+    name = "sas3ircu"
+    symlink_bin = TOOLS_DIR / "sas3ircu"
 
 
 class VendorHelper:
     """Helper to install vendor's tools."""
 
     @property
-    def strategies(self) -> t.Dict[str, VendorStrategyABC]:
+    def strategies(self) -> t.Dict[str, StrategyABC]:
         """Define strategies for every vendor's tools."""
         return {
-            "storecli-deb": StorCLIStrategy(),
+            "storcli-deb": StorCLIStrategy(),
+            "perccli-deb": PercCLIStrategy(),
+            "sas2ircu-bin": SAS2IRCUStrategy(),
+            "sas3ircu-bin": SAS3IRCUStrategy(),
         }
 
-    def fetch_tools(self, resources: Resources) -> t.List[str]:
+    def fetch_tools(self, resources: Resources) -> t.Dict[str, Path]:
         """Fetch resource from juju if it's VENDOR_TOOLS."""
+        fetch_tools: t.Dict[str, Path] = {}
         # Fetch all tools from juju resources
         for tool in VENDOR_TOOLS:
             try:
-                resources.fetch(tool)
+                path = resources.fetch(tool)
+                fetch_tools[tool] = path
             except ModelError:
                 logger.warning("Fail to fetch tool: %s", tool)
 
-        fetch_tools = [
-            name
-            for name, path in resources._paths.items()  # pylint: disable=W0212
-            if name in VENDOR_TOOLS and path is not None
-        ]
         return fetch_tools
 
     def install(self, resources: Resources) -> None:
         """Install tools."""
         fetch_tools = self.fetch_tools(resources)
-        for name, path in resources._paths.items():  # pylint: disable=W0212
-            if name in fetch_tools:
-                strategy = self.strategies.get(name)
-                if strategy and path:
-                    strategy.install(name, path)
-                else:
-                    logger.warning("Could not find install strategy for tool %s", name)
+        for name, path in fetch_tools.items():
+            strategy = self.strategies.get(name)
+            if isinstance(strategy, TPRStrategyABC) and path:
+                strategy.install(name, path)
+            else:
+                logger.warning("Could not find install strategy for tool %s", name)
 
     def remove(self, resources: Resources) -> None:
         """Execute all remove strategies."""
         for name in resources._paths.keys():  # pylint: disable=W0212
             if name in VENDOR_TOOLS:
                 strategy = self.strategies.get(name)
-                if strategy:
+                if isinstance(strategy, TPRStrategyABC):
                     strategy.remove()
                     logger.info("Remove resource: %s", name)
                 else:
