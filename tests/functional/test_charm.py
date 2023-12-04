36be58604e5dc5e6b5736a94e4595df6eccb84b1
Ashley James <ashley.james@canonical.com>
2023-12-04 11:52:25 +0530

    Set asyncio mark in test module.
    func tests for juju 2.9/focal were failing. This was because the asyncio
    marker was not added to the tests.
    
    This change adds the markers automatically to all the tests by setting
    `pytestmark`.

--- tests/functional/test_charm.py
+++ tests/functional/test_charm.py
@@ -14,6 +14,7 @@ import yaml
 from pytest_operator.plugin import OpsTest
 
 logger = logging.getLogger(__name__)
+pytestmark = pytest.mark.asyncio(scope="module")
 
 METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 APP_NAME = METADATA["name"]
