9c9449bf3f6bd3d1ba47cbbd067620d1a305d29e
Ashley James <ashley.james@canonical.com>
2024-02-19 11:13:15 +0530

    feat: Add pytest config option to accept collectors
    Also adds "realhw" marker which when applied to test functions, will
    skip them in case no collectors are provided via the CLI option.

--- tests/functional/conftest.py
+++ tests/functional/conftest.py
@@ -1,6 +1,9 @@
 import logging
 
 import pytest
+from async_lru import alru_cache
+
+from charm import HardwareObserverCharm
 
 log = logging.getLogger(__name__)
 
@@ -18,6 +21,15 @@ class SyncHelper:
         }
         return results
 
+    @staticmethod
+    @alru_cache
+    async def get_metrics_output(ops_test, unit_name):
+        """Return prometheus metric output from endpoint on unit."""
+        port = HardwareObserverCharm.DEFAULT_EXPORTER_PORT
+        command = f"curl localhost:{port}"
+        results = await SyncHelper.run_command_on_unit(ops_test, unit_name, command)
+        return results
+
 
 def pytest_addoption(parser):
     parser.addoption(
@@ -28,10 +40,49 @@ def pytest_addoption(parser):
         help="Set series for the machine units",
     )
 
+    parser.addoption(
+        "--collectors",
+        nargs="+",
+        type=str.lower,
+        default="",
+        choices=[
+            "ipmi_dcmi",
+            "ipmi_sel",
+            "ipmi_sensor",
+            "redfish",
+            "mega_raid",
+            "poweredge_raid",
+            "lsi_sas_2",
+            "lsi_sas_3",
+        ],
+        help="Provide space-separated collectors for testing with real hardware.",
+    )
+
 
 @pytest.fixture(scope="module")
 def series(request):
     return request.config.getoption("--series")
+
+
+@pytest.fixture(scope="module")
+def provided_collectors(request):
+    return request.config.getoption("collectors")
+
+
+def pytest_configure(config):
+    config.addinivalue_line("markers", "realhw: mark test as requiring real hardware to run.")
+
+
+def pytest_collection_modifyitems(config, items):
+    if config.getoption("collectors"):
+        # --collectors provided, do not skip tests
+        return
+    skip_real_hw = pytest.mark.skip(
+        reason="Hardware dependent test. Provide collectors with the --collectors option."
+    )
+    for item in items:
+        if "realhw" in item.keywords:
+            item.add_marker(skip_real_hw)
 
 
 @pytest.fixture(scope="module")
