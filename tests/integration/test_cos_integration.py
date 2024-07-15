646d95e7e935945e24b3e735b137a488fa701fbe
Xuhui Zhu <simon.zhu@canonical.com>
2024-07-15 19:46:38 -0400

    fixed test to use internal traefik address

--- tests/integration/test_cos_integration.py
+++ tests/integration/test_cos_integration.py
@@ -38,10 +38,11 @@ async def test_alerts(ops_test: OpsTest, lxd_model, k8s_model):
 
     model_name = ops_test.model_name
 
-    model_status = await k8s_model.get_status()
-    traefik_ip = model_status["applications"]["traefik"].public_address
-
-    prometheus_alerts_endpoint = f"http://{traefik_ip}/{model_name}-prometheus-0/api/v1/alerts"
+    returncode, stdout, stderr = await ops_test.run("juju", "run", "traefik/0", "show-proxied-endpoints")
+    result_json = stdout.split('proxied-endpoints: ')[1].strip().strip('\'')
+    internal_address_info = json.loads(result_json)
+    prometheus_internal_url = internal_address_info.get("traefik").get("url")
+    prometheus_alerts_endpoint = f"{prometheus_internal_url}/{model_name}-prometheus-0/api/v1/alerts"
 
     cmd = ["curl", prometheus_alerts_endpoint]
 
