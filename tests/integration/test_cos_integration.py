bb3f3fe29478a6d83eb42c63d8b115179a74e3a6
Xuhui Zhu <simon.zhu@canonical.com>
2024-07-17 17:19:27 -0400

    fix json load

--- tests/integration/test_cos_integration.py
+++ tests/integration/test_cos_integration.py
@@ -40,11 +40,11 @@ async def test_alerts(ops_test: OpsTest, lxd_model, k8s_model):
 
     # model_status = await k8s_model.get_status()
     returncode, stdout, stderr = await ops_test.run("juju", "run", "--format", "json", "traefik/0", "show-proxied-endpoints",)
-    print("ops_result:"+stdout)
-    address_info = json.loads(stdout)
-    traefik_url = address_info.get("traefik").get("url")
-    prometheus_alerts_endpoint = f"{traefik_url}/{model_name}-prometheus-0/api/v1/alerts"
-    logger.info(prometheus_alerts_endpoint)
+    data = json.loads(stdout)
+    print(data["traefik/0"]["results"]["proxied-endpoints"])
+    proxied_endpoints = json.loads(data["traefik/0"]["results"]["proxied-endpoints"])
+    prometheus_url = proxied_endpoints["prometheus/0"]["url"]
+    prometheus_alerts_endpoint = f"{prometheus_url}/api/v1/alerts"
     cmd = ["curl", prometheus_alerts_endpoint]
 
     # Sometimes alerts take some time to show after the metrics are exposed on the host.
