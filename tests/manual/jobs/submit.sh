bfface92d50f6947e3d5b06ab7ecc7dbe5ae7bdb
Chi Wai Chan <chiwai.chan@canonical.com>
2025-03-18 11:35:43 +0800

    Fix submit.sh
    Snap cannot access /tmp

--- tests/manual/jobs/submit.sh
+++ tests/manual/jobs/submit.sh
@@ -11,7 +11,9 @@ if [ ! -d "$JOB" ] || [ -z "$SERIES" ] || [ -z "$SSH_IMPORT_ID" ]; then
   exit 1
 fi
 
-TEMPFILE="$(mktemp)"
+# testflinger cannot access /tmp file because it does not have necessary permission
+TEMPFILE="./.tmp-job.yaml"
+touch $TEMPFILE
 
 sed -e "s/<SERIES>/$SERIES/g" -e "s/<SSH_IMPORT_ID>/$SSH_IMPORT_ID/g" "$JOB/job.tpl.yaml" | tee "$TEMPFILE"
 
