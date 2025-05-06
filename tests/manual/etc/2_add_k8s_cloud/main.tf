7a5a624db30004b2c74de9497e224bf83cb17028
Gabriel Cocenza <gabriel.cocenza@canonical.com>
2025-05-06 16:12:48 -0300

    - wait for microk8s before adding k8s cloud - add base for hwo

--- tests/manual/etc/2_add_k8s_cloud/main.tf
+++ tests/manual/etc/2_add_k8s_cloud/main.tf
@@ -9,7 +9,13 @@ terraform {
 
 provider "juju" {}
 
+resource "null_resource" "wait_for_microk8s" {
+  provisioner "local-exec" {
+    command = "sudo microk8s status --wait-ready"
+  }
+}
 resource "juju_kubernetes_cloud" "k8s_cloud" {
   name              = "k8s"
   kubernetes_config = file(var.kube_config)
+  depends_on        = [null_resource.wait_for_microk8s]
 }
