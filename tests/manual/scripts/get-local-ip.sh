1cc6a5bed14685028f13a764dd23930ac6de1719
Quang Ngo <quang.ngo@canonical.com>
2026-04-21 11:57:36 +1000

    add condition for noble in get-local-ip.sh

--- tests/manual/scripts/get-local-ip.sh
+++ tests/manual/scripts/get-local-ip.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-ip -4 -j a sho dev br0 | jq -r .[].addr_info[0].local
+UBUNTU_MAJOR=$(lsb_release -sr | cut -d. -f1)
+if [ "$UBUNTU_MAJOR" -lt 24 ]; then
+    # br0 bridges the physical NIC so its IP equals the external IP
+    ip -4 -j a sho dev br0 | jq -r .[].addr_info[0].local
+else
+    # On Noble+, lxdbr0 is NAT-only so its gateway IP doesn't match the machine's real IP;
+    # Here use the routing-preferred source address instead
+    ip -4 -j route get 2.2.2.2 | jq -r '.[] | .prefsrc'
+fi
